637a972063363154a88830c5619071a063de2fa8
francois <francois@6033c753-d401-9f41-9dea-e22d9fdc6268>
2012-07-27 09:51:34 +0000

    git-svn-id: svn://localhost/fileoperations/trunk@8 6033c753-d401-9f41-9dea-e22d9fdc6268

--- SearchInFiles/Program.cs
+++ SearchInFiles/Program.cs
@@ -8,30 +8,48 @@ using System.Drawing;
 
 namespace SearchInFiles
 {
-    static class Program
-    {
-        const string ThisAppKeyName = "SearchInFiles";
+	static class Program
+	{
+		const string ThisAppKeyName = "SearchInFiles";
 
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
 			var args = Environment.GetCommandLineArgs();
 			//args = new string[] { "", @"C:\Users\francois\Dropbox\Dev\VSprojects\SharedClasses" };//@"C:\Users\francois\Dropbox\Temp\WaterSkills\Source" };
 
-            if (Application.ExecutablePath.StartsWith(@"C:\Program Files", StringComparison.InvariantCultureIgnoreCase))
-                SharedClasses.RegistryInterop.AddCommandToFolder(
-                    "SearchInFiles",
-                    "\"" + Environment.GetCommandLineArgs()[0] + "\" \"%1\"",
-                    "Search in files",
-                    "\"" + Environment.GetCommandLineArgs()[0] + "\"",//"shell32.dll,-320",
-                    null,//"SearchInFiles",
-                    ThisAppKeyName);
+			if (Application.ExecutablePath.StartsWith(@"C:\Program Files", StringComparison.InvariantCultureIgnoreCase))
+			{
+
+				string iconPath = "\"" + Environment.GetCommandLineArgs()[0] + "\"";//"shell32.dll,-320",;
+				bool added = RegistryInterop.AddSubMenuCommands(new RegistryInterop.MainContextMenuItem(
+					new List<string>() { "Folder" },
+					"Search in files",
+					//"Hex converting",
+					iconPath,
+					new List<RegistryInterop.SubContextMenuItem>()
+				{
+					new RegistryInterop.SubContextMenuItem("_SearchInFiles.SearchInFiles", "Search in files", iconPath, "\"" + Environment.GetCommandLineArgs()[0] + "\" \"%1\"")
+				}));
+
+				if (!added)
+					UserMessages.ShowWarningMessage("Cannot register application " + ThisAppKeyName + " to right-click menu, unable to write registry");
+
+				//SharedClasses.RegistryInterop.AddCommandToFolder(
+				//    "SearchInFiles",
+				//    "\"" + Environment.GetCommandLineArgs()[0] + "\" \"%1\"",
+				//    "Search in files",
+				//    "\"" + Environment.GetCommandLineArgs()[0] + "\"",//"shell32.dll,-320",
+				//    null,//"SearchInFiles",
+				//    ThisAppKeyName);
+			}
+
 			if (args.Length <= 1)
 				UserMessages.ShowWarningMessage("This program (" + ThisAppKeyName + ") needs a commandline argument which should be the root directory to search in");
 			else
@@ -51,6 +69,6 @@ namespace SearchInFiles
 					Application.Run(mainform);
 				}
 			}
-        }
-    }
+		}
+	}
 }
